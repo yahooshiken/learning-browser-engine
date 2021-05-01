9edb893c191120a5e8b60c0ef63c2ebd7303b291
Hoshi <hoshimure.47@gmail.com>
2021-05-02 01:53:25 +0900

    :+1: Reading a file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 pub mod dom;
 pub mod html_parser;
 
+use std::fs;
+
 fn main() {
-    let dom = html_parser::parse("<html></html>".to_string());
+    let filename = "index.html";
+    let html = fs::read_to_string(filename).expect("Something went wrong");
+    let dom = html_parser::parse(html);
+
     println!("{:?}", dom);
 }
